0aa2f1ca1701db06e5406cc0db8c2299ada0e788
jfabraxas <git@stackblitz.com>
2024-10-08 20:00:38 +0200

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Multitenant Next.js Platform with Payload CMS',
+  description: 'A multitenant platform where users can edit content with Payload CMS',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
